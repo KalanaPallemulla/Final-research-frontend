fc0a0ef6366fa39342fbfd138e68d2daea018f6c
Kalana <kalanaheshan47@gmail.com>
2023-05-05 13:03:13 +0530

    remove: duplicate folder

--- Frontend/src/appRedux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import articleSlice from "../Pages/redux/articles/reducer";
-
-const rootReducer = combineReducers({
-  article: articleSlice.reducer,
-});
-
-export default rootReducer;
--- Frontend/src/environment.js
+++
@@ -1,2 +0,0 @@
-// export const base_url = "http://localhost:8000/api/";
-export const base_url = "https://research-backend47.herokuapp.com/api/";
